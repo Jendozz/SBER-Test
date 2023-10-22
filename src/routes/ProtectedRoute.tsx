98fa5fe2817c9db4c96ed79fe7b79e1a5082d7f9
Evgeniy Naumov <evegeninaum@gmail.com>
2023-10-22 22:41:27 +0400

    bugfix/1.0.0 - fix redirect

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
-import { useLayoutEffect, type FC, type ReactNode } from 'react';
+import { type FC, type ReactNode } from 'react';
 
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import { useAppSelector } from '@store/hooks';
 import { selectIsLoggedIn } from '@store/reducers/authSlice/selectors';
@@ -13,13 +13,9 @@ interface IProtectedRouteProps {
 
 export const ProtectedRoute: FC<IProtectedRouteProps> = ({ children }) => {
     const isLoggedIn = useAppSelector(selectIsLoggedIn);
-    const navigate = useNavigate();
 
-    useLayoutEffect(() => {
-        if (!isLoggedIn) {
-            navigate(ERoutePath.LOGIN, { replace: true });
-        }
-    }, [isLoggedIn, navigate]);
-
+    if (!isLoggedIn) {
+        return <Navigate replace to={ERoutePath.LOGIN} />;
+    }
     return <>{children}</>;
 };
